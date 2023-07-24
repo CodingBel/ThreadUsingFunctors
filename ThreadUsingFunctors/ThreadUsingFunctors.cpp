11dc0771f46fbf39177e367cb0f1adaa30da27f1
Abel <belaashu23@gmail.com>
2023-07-24 14:47:11 +0200

    Add project files.

---
+++ ThreadUsingFunctors/ThreadUsingFunctors.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <thread>
+
+class Abel{
+	char printChar; 
+public: 
+	void operator()(int num1, int num2) {
+		for (int i = num1; i < num2; i++) {
+			std::cout << "@"; 
+		}
+	}
+
+	void operator()(int num1, int num2, int num3) {
+		if (num1 < num2)
+			printChar = '&';
+		else
+			printChar = '%'; 
+
+		for (int i = num1; i < num3; i++)
+			std::cout << printChar; 
+	}
+}; 
+
+class Bel {
+public: 
+	void operator()(int num1, int num2) {
+		for (int i = num1; i < num2; i++) {
+			std::cout << "#"; 
+		}
+	}
+};
+
+int main() {
+	Abel obj1; 
+	Bel obj2; 
+	Abel obj3; 
+
+	std::thread Thread1(obj1, 0, 10);
+	std::thread Thread2(obj2, 0, 10);
+	std::thread Thread3(obj3, 4, 1, 12);
+
+	Thread1.join(); 
+	Thread2.join(); 
+	Thread3.join(); 
+
+	return 0;
+}
